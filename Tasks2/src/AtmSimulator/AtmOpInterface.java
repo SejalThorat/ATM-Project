7ef1932b46c97f0729ddb223852d2316bdd6a5f1
Sejal Thoart <thoratsejal248@gmail.com>
2024-03-03 13:42:49 +0530

    Add Files Via upload

---
+++ Tasks2/src/AtmSimulator/AtmOpInterface.java
@@ -0,0 +1,10 @@
+package AtmSimulator;
+
+public interface AtmOpInterface {
+
+	   public void viewBalance();
+	    public void withdrawAmount(double withdrawAmount);
+	    public void depositAmount(double depositAmount );
+	    public void viewMiniStatement();
+}
+
